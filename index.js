ccdb5b249317dc208ef072ed83531a297031f6af
f957-cua <ivanfiliptschik00@gmail.com>
2023-06-21 19:26:31 +0300

    vercel_step_3

---
+++ index.js
@@ -0,0 +1,24 @@
+import app from "./app.js";
+import mongoose from "mongoose";
+import env from "dotenv";
+
+env.config();
+
+const { DB_URL, PORT = 5000 } = process.env;
+
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => {
+      console.log(`Backend started on Port: ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e.message);
+    process.exit(1);
+  }
+}
+
+startApp();
